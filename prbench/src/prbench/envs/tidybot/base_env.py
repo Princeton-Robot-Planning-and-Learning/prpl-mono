12916ae6cf03a86c48e3cd2667d090a601bcedc0
Vaibhav Saxena <saxena.vaibhav96@gmail.com>
2025-10-07 17:26:01 -0400

    added dynamic 3D object centric base env

---
+++ prbench/src/prbench/envs/tidybot/base_env.py
@@ -0,0 +1,33 @@
+"""Base class for Dynamic3D robot environments."""
+
+from dataclasses import dataclass
+
+# from pyparsing import TypeVar
+from gymnasium.spaces import Space
+
+# from relational_structs.common import Array
+from relational_structs.object_centric_state import ObjectCentricState
+
+from prbench.core import ObjectCentricPRBenchEnv, PRBenchEnvConfig
+from prbench.envs.tidybot.mujoco_utils import MjAct
+
+
+@dataclass(frozen=True)
+class TidyBot3DConfig(PRBenchEnvConfig):
+    """Configuration for TidyBot3D environment."""
+
+    control_frequency: int = 20
+    horizon: int = 1000
+    camera_names: list[str] | None = None
+    camera_width: int = 640
+    camera_height: int = 480
+    show_viewer: bool = False
+
+
+class ObjectCentricDynamic3DRobotEnv(
+    ObjectCentricPRBenchEnv[ObjectCentricState, MjAct, TidyBot3DConfig]
+):
+    """Base class for Dynamic3D robot environments."""
+
+    def _create_action_space(self, config: TidyBot3DConfig) -> Space[MjAct]:
+        """Create action space for TidyBot's control interface."""
