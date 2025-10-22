2d1b15560d4ebd988c28b3eb376d5a2d9aef45ba
Tom Silver <tomssilver@gmail.com>
2025-10-22 16:48:02 -0400

    oops

--- prpl-tidybot/src/prpl_tidybot/real_env.py
+++ prpl-tidybot/src/prpl_tidybot/real_env.py
@@ -3,7 +3,6 @@
 from typing import Any, SupportsFloat
 
 import gymnasium
-<<<<<<< HEAD
 from gymnasium.core import RenderFrame
 
 from prpl_tidybot.interface import Interface
@@ -34,46 +33,6 @@ class RealTidyBotEnv(gymnasium.Env[TidyBotObservation, TidyBotAction]):
     ) -> tuple[TidyBotObservation, SupportsFloat, bool, bool, dict[str, Any]]:
         # Coming soon!
         obs = self._get_obs()
-||||||| b8b1fde
-=======
-import numpy as np
-import spatialmath
-from gymnasium.core import RenderFrame
-
-from prpl_tidybot.structs import CAMERA_DIMS, TidyBotAction, TidyBotObservation
-
-
-class RealTidyBotEnv(gymnasium.Env[TidyBotObservation, TidyBotAction]):
-    """Gymnasium environment for the real TidyBot++."""
-
-    def reset(
-        self,
-        *,
-        seed: int | None = None,
-        options: dict[str, Any] | None = None,
-    ) -> tuple[TidyBotObservation, dict[str, Any]]:  # type: ignore
-        # Coming soon!
-        obs = TidyBotObservation(
-            arm_conf=[0.0] * 7,
-            base_pose=spatialmath.SE2(x=0, y=0, theta=0),
-            gripper=0.0,
-            wrist_camera=np.zeros(CAMERA_DIMS, dtype=np.uint8),
-            base_camera=np.zeros(CAMERA_DIMS, dtype=np.uint8),
-        )
-        return obs, {}
-
-    def step(
-        self, action: TidyBotAction
-    ) -> tuple[TidyBotObservation, SupportsFloat, bool, bool, dict[str, Any]]:
-        # Coming soon!
-        obs = TidyBotObservation(
-            arm_conf=[0.0] * 7,
-            base_pose=spatialmath.SE2(x=0, y=0, theta=0),
-            gripper=0.0,
-            wrist_camera=np.zeros(CAMERA_DIMS, dtype=np.uint8),
-            base_camera=np.zeros(CAMERA_DIMS, dtype=np.uint8),
-        )
->>>>>>> 233cfb0e77b9ec83ec12173a5cb3b37e94c86545
         return obs, 0.0, False, False, {}
 
     def render(self) -> RenderFrame | list[RenderFrame] | None:
