eb5abbb4083f36ca4a2164c0c73ff6ee45d79f3a
Tom Silver <tomssilver@gmail.com>
2025-10-22 16:17:52 -0400

    wip

--- prpl-tidybot/src/prpl_tidybot/structs.py
+++ prpl-tidybot/src/prpl_tidybot/structs.py
@@ -1,15 +1,22 @@
 """Data structures."""
 
 from dataclasses import dataclass
+import spatialmath
 
 
 @dataclass(frozen=True)
-class Dog:
-    """An example class."""
+class TidyBotObservation:
+    """Raw observations from the TidyBot environment."""
 
-    name: str
-    breed: str
+    arm_conf: list[float]  # 7-DOF joints
+    base_pose: spatialmath.SE2  # base pose for the robot
+    gripper: float  # 1 = closed, 0 = open
 
-    def is_good(self) -> bool:
-        """All dogs are good."""
-        return True
+
+@dataclass(frozen=True)
+class TidyBotAction:
+    """Low-level joint and base commands for the real TidyBot environment."""
+
+    arm_goal: list[float]  # absolute arm position
+    base_local_goal: spatialmath.SE2  # relative to current base pose
+    gripper_goal: float  # absolute
