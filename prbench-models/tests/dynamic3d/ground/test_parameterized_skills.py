db0b01cac5285e31ee2f8d1e5a5b47a3302c2837
Tom Silver <tomssilver@gmail.com>
2025-10-26 08:20:55 -0400

    wip

---
+++ prbench-models/tests/dynamic3d/ground/test_parameterized_skills.py
@@ -0,0 +1,6 @@
+"""Tests for ground parameterized skills."""
+
+from prbench_models.dynamic3d.ground.parameterized_skills import create_lifted_controllers
+
+def test_base_motion_skill():
+    """Tests for base motion skill in ground environment."""
