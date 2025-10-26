0d17dc82e5c14a800c953b1dffdb0070871cc758
Tom Silver <tomssilver@gmail.com>
2025-10-26 10:14:40 -0400

    wip

--- prbench-models/src/prbench_models/dynamic3d/utils.py
+++ prbench-models/src/prbench_models/dynamic3d/utils.py
@@ -1,11 +1,12 @@
 """Utils for tidybot environments."""
 
-from prbench.envs.dynamic3d.object_types import MujocoObjectType
+from prbench.envs.dynamic3d.object_types import MujocoObjectType, MujocoRobotObjectType
 from relational_structs import (
     Object,
     ObjectCentricState,
 )
 from spatialmath import SE2, UnitQuaternion
+from tomsgeoms2d.structs import Geom2D
 
 
 def get_overhead_object_se2_pose(state: ObjectCentricState, obj: Object) -> SE2:
@@ -24,3 +25,16 @@ def get_overhead_object_se2_pose(state: ObjectCentricState, obj: Object) -> SE2:
     rpy = q.rpy()
     yaw = rpy[2]
     return SE2(x, y, yaw)
+
+
+def get_overhead_robot_se2_pose(state: ObjectCentricState, obj: Object) -> SE2:
+    """Get the top-down SE2 pose for an object in a dynamic3D state."""
+    assert obj.is_instance(MujocoRobotObjectType)
+    x = state.get(obj, "pos_base_x")
+    y = state.get(obj, "pos_base_y")
+    yaw = state.get(obj, "pos_base_rot")
+    return SE2(x, y, yaw)
+
+
+def get_overhead_geom2ds(state: ObjectCentricState) -> dict[str, Geom2D]:
+    """Get a mapping from object name to Geom2D from an overhead perspective."""
--- prbench-models/tests/dynamic3d/test_dynamic3d_utils.py
+++ prbench-models/tests/dynamic3d/test_dynamic3d_utils.py
@@ -4,7 +4,10 @@ import numpy as np
 import prbench
 from relational_structs.spaces import ObjectCentricBoxSpace
 
-from prbench_models.dynamic3d.utils import get_overhead_object_se2_pose
+from prbench_models.dynamic3d.utils import (
+    get_overhead_object_se2_pose,
+    get_overhead_robot_se2_pose,
+)
 
 prbench.register_all_environments()
 
@@ -35,3 +38,25 @@ def test_get_overhead_object_se2_pose():
     assert np.isclose(pose1.x + 1, pose3.x)
     assert np.isclose(pose1.y, pose3.y)
     assert np.isclose(pose1.theta(), pose3.theta())
+
+
+def test_get_overhead_robot_se2_pose():
+    """Tests for get_overhead_robot_se2_pose()."""
+
+    # Get a real object-centric state.
+    env = prbench.make("prbench/TidyBot3D-ground-o1-v0")
+    assert isinstance(env.observation_space, ObjectCentricBoxSpace)
+    obs, _ = env.reset(seed=123)
+    state1 = env.observation_space.devectorize(obs)
+    robot = state1.get_object_from_name("robot")
+
+    # Extract the initial SE2 pose.
+    pose1 = get_overhead_robot_se2_pose(state1, robot)
+
+    # Move the object x should have an effect.
+    state2 = state1.copy()
+    state2.set(robot, "pos_base_x", state1.get(robot, "pos_base_x") + 1.0)
+    pose2 = get_overhead_robot_se2_pose(state2, robot)
+    assert np.isclose(pose1.x + 1, pose2.x)
+    assert np.isclose(pose1.y, pose2.y)
+    assert np.isclose(pose1.theta(), pose2.theta())
