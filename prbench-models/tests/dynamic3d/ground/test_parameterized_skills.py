42e27afefa7004ca9cd9e42e58caa8bd8775c4ec
Tom Silver <tomssilver@gmail.com>
2025-10-26 08:34:24 -0400

    wip

--- prbench-models/tests/dynamic3d/ground/test_parameterized_skills.py
+++ prbench-models/tests/dynamic3d/ground/test_parameterized_skills.py
@@ -2,5 +2,27 @@
 
 from prbench_models.dynamic3d.ground.parameterized_skills import create_lifted_controllers
 
-def test_base_motion_skill():
-    """Tests for base motion skill in ground environment."""
+import prbench
+from conftest import MAKE_VIDEOS
+from gymnasium.wrappers import RecordVideo
+
+
+prbench.register_all_environments()
+
+
+def test_move_to_target_controller_one_cube():
+    """Test move-to-target controller in ground environment with 1 cube."""
+
+    num_cubes = 1
+
+    env = prbench.make(f"prbench/TidyBot3D-ground-o{num_cubes}-v0", render_mode="rgb_array")
+
+    if MAKE_VIDEOS:
+        env = RecordVideo(
+            env, "unit_test_videos", name_prefix=f"TidyBot3D-ground-o{num_cubes}"
+        )
+
+    controllers = create_lifted_controllers(env.action_space)
+    move_to_target_controller = controllers["move_to_target"]
+
+    import ipdb; ipdb.set_trace()
