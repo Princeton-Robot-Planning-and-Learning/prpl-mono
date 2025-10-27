fbeacb0d72c704a0eb34a8a421cba4521ecc1e74
Yixuan Huang <yixuanhuang98@gmail.com>
2025-10-27 14:48:54 -0400

    add control loop for base motion planning

--- prbench-models/tests/dynamic3d/test_dynamic3d_utils.py
+++ prbench-models/tests/dynamic3d/test_dynamic3d_utils.py
@@ -1,13 +1,18 @@
 """Test utils for dynamic3d models."""
 
+import time
+
+import imageio.v2 as iio
 import numpy as np
 import prbench
 from matplotlib import pyplot as plt
+from prpl_utils.utils import fig2data, get_signed_angle_distance
 from relational_structs.spaces import ObjectCentricBoxSpace
 from spatialmath import SE2
 from tomsgeoms2d.structs import Rectangle
 
 from prbench_models.dynamic3d.utils import (
+    get_bounding_box,
     get_overhead_geom2ds,
     get_overhead_object_se2_pose,
     get_overhead_robot_se2_pose,
@@ -125,63 +130,81 @@ def test_run_base_motion_planning():
     )
     assert base_motion_plan is not None
 
-    # Uncomment to debug.
-    # import imageio.v2 as iio
-    # from prpl_utils.utils import fig2data
-    # from prbench_models.dynamic3d.utils import get_bounding_box
-    # from prpl_utils.utils import get_signed_angle_distance
-
-    # fig, ax = plot_overhead_scene(
-    #     state,
-    #     min_x=x_bounds[0],
-    #     max_x=x_bounds[1],
-    #     min_y=y_bounds[0],
-    #     max_y=y_bounds[1],
-    # )
-    # robot = state.get_object_from_name("robot")
-    # robot_width, robot_height, _ = get_bounding_box(state, robot)
-    # for pose in base_motion_plan:
-    #     robot_geom = Rectangle.from_center(
-    #         pose.x,
-    #         pose.y,
-    #         robot_width,
-    #         robot_height,
-    #         rotation_about_center=pose.theta(),
-    #     )
-    #     robot_geom.plot(ax, fc="none", ec="gray", linestyle="dashed")
-    # ax.set_title("Motion Planning Example")
-    # plt.tight_layout()
-    # img = fig2data(fig)
-    # outfile = "base_motion_planning.png"
-    # iio.imsave(outfile, img)
-    # print(f"Wrote out to {outfile}")
+    fig, ax = plot_overhead_scene(
+        state,
+        min_x=x_bounds[0],
+        max_x=x_bounds[1],
+        min_y=y_bounds[0],
+        max_y=y_bounds[1],
+    )
+    robot = state.get_object_from_name("robot")
+    robot_width, robot_height, _ = get_bounding_box(state, robot)
+    for pose in base_motion_plan:
+        robot_geom = Rectangle.from_center(
+            pose.x,
+            pose.y,
+            robot_width,
+            robot_height,
+            rotation_about_center=pose.theta(),
+        )
+        robot_geom.plot(ax, fc="none", ec="gray", linestyle="dashed")
+    ax.set_title("Motion Planning Example")
+    plt.tight_layout()
+    img = fig2data(fig)
+    outfile = "base_motion_planning.png"
+    iio.imsave(outfile, img)
+    print(f"Wrote out to {outfile}")
 
     # # NOTE: this does not work. We need to instead do reactive control to follow the
     # # motion plan, possibly using the base_controller. The reason this doesn't work
     # # is because the position control action space is unreliable.
-    # imgs = []
-    # for t in range(1, len(base_motion_plan)):
-    #     previous_pose = base_motion_plan[t - 1]
-    #     pose = base_motion_plan[t]
-    #     dx = pose.x - previous_pose.x
-    #     dy = pose.y - previous_pose.y
-    #     drot = get_signed_angle_distance(pose.theta(), previous_pose.theta())
-    #     action = np.zeros(11, dtype=np.float32)
-    #     action[0] = dx
-    #     action[1] = dy
-    #     action[2] = drot
-    #     assert env.action_space.contains(action)
-    #     obs, _, _, _, _ = env.step(action)
-    #     state = env.observation_space.devectorize(obs)
-    #     print("Expected x, y, rot:", pose.x, pose.y, pose.theta())
-    #     print(
-    #         "Actual x, y, rot:",
-    #         state.get(robot, "pos_base_x"),
-    #         state.get(robot, "pos_base_y"),
-    #         state.get(robot, "pos_base_rot"),
-    #     )
-    #     img = env.render()
-    #     imgs.append(img)
-    # outfile = "base_motion_planning.mp4"
-    # iio.mimsave(outfile, imgs)
-    # print(f"Wrote out to {outfile}")
+    imgs = []
+    for t in range(1, len(base_motion_plan)):
+        pose = base_motion_plan[t]
+        max_control_steps = 10
+        tolerance = 1e-2
+        control_period = 0.1  # 10hz
+        for control_step in range(max_control_steps):
+            previous_pose = SE2(
+                state.get(robot, "pos_base_x"),
+                state.get(robot, "pos_base_y"),
+                state.get(robot, "pos_base_rot"),
+            )
+            dx = pose.x - previous_pose.x
+            dy = pose.y - previous_pose.y
+            drot = get_signed_angle_distance(pose.theta(), previous_pose.theta())
+            action = np.zeros(11, dtype=np.float32)
+            action[0] = dx
+            action[1] = dy
+            action[2] = drot
+            assert env.action_space.contains(action)
+
+            obs, _, _, _, _ = env.step(action)
+            state = env.observation_space.devectorize(obs)
+            print("Expected x, y, rot:", pose.x, pose.y, pose.theta())
+            print(
+                "Actual x, y, rot:",
+                state.get(robot, "pos_base_x"),
+                state.get(robot, "pos_base_y"),
+                state.get(robot, "pos_base_rot"),
+            )
+            time.sleep(
+                control_period
+            )  # sleep for 100ms to allow the action to be executed
+            if (
+                np.isclose(state.get(robot, "pos_base_x"), pose.x, atol=tolerance)
+                and np.isclose(state.get(robot, "pos_base_y"), pose.y, atol=tolerance)
+                and np.isclose(
+                    state.get(robot, "pos_base_rot"), pose.theta(), atol=tolerance
+                )
+            ):
+                print(
+                    f"Reached target pose {pose.x}, {pose.y}, {pose.theta()} "
+                    f"in {control_step + 1} steps"
+                )
+                break
+            img = env.render()
+            imgs.append(img)
+    outfile = "base_motion_planning.mp4"
+    iio.mimsave(outfile, imgs)
+    print(f"Wrote out to {outfile}")
