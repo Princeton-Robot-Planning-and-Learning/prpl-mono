28a59f94aadfae899ea9f836909fa701fa7f7fc2
Yixuan Huang <yixuanhuang98@gmail.com>
2025-10-28 11:46:13 -0400

    remove unused comments

--- prpl-tidybot/src/prpl_tidybot/arm_server.py
+++ prpl-tidybot/src/prpl_tidybot/arm_server.py
@@ -80,20 +80,3 @@ if __name__ == '__main__':
     server = manager.get_server()
     print(f'Arm manager server started at {ARM_RPC_HOST}:{ARM_RPC_PORT}')
     server.serve_forever()
-    # import numpy as np
-    # from constants import POLICY_CONTROL_PERIOD
-    # manager = ArmManager(address=(ARM_RPC_HOST, ARM_RPC_PORT), authkey=RPC_AUTHKEY)
-    # manager.connect()
-    # arm = manager.Arm()
-    # try:
-    #     arm.reset()
-    #     for i in range(50):
-    #         arm.execute_action({
-    #             'arm_pos': np.array([0.135, 0.002, 0.211]),
-    #             'arm_quat': np.array([0.706, 0.707, 0.029, 0.029]),
-    #             'gripper_pos': np.zeros(1),
-    #         })
-    #         print(arm.get_state())
-    #         time.sleep(POLICY_CONTROL_PERIOD)  # Note: Not precise
-    # finally:
-    #     arm.close()
--- prpl-tidybot/src/prpl_tidybot/base_server.py
+++ prpl-tidybot/src/prpl_tidybot/base_server.py
@@ -58,16 +58,3 @@ if __name__ == '__main__':
     server = manager.get_server()
     print(f'Base manager server started at {BASE_RPC_HOST}:{BASE_RPC_PORT}')
     server.serve_forever()
-    # import numpy as np
-    # from constants import POLICY_CONTROL_PERIOD
-    # manager = BaseManager(address=(BASE_RPC_HOST, BASE_RPC_PORT), authkey=RPC_AUTHKEY)
-    # manager.connect()
-    # base = manager.Base()
-    # try:
-    #     base.reset()
-    #     for i in range(50):
-    #         base.execute_action({'base_pose': np.array([(i / 50) * 0.5, 0.0, 0.0])})
-    #         print(base.get_state())
-    #         time.sleep(POLICY_CONTROL_PERIOD)  # Note: Not precise
-    # finally:
-    #     base.close()
