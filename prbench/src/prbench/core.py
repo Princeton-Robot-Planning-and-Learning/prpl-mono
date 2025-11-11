c016bf06ddb81105b001bbd97167a5d3e588afc1
Bowen Li <bowenli2@andrew.cmu.edu>
2025-11-11 11:47:54 -0500

    fixed obs float64 issue, ready for new demo collection

--- prbench/src/prbench/core.py
+++ prbench/src/prbench/core.py
@@ -162,10 +162,11 @@ class ConstantObjectPRBenchEnv(gymnasium.Env[NDArray[Any], NDArray[Any]]):
         assert isinstance(
             self._object_centric_env.observation_space, ObjectCentricStateSpace
         )
-        self.observation_space = self._object_centric_env.observation_space.to_box(
-            self._constant_objects, self._object_centric_env.type_features
-        )
         self.action_space = self._object_centric_env.action_space
+        self.observation_space = self._object_centric_env.observation_space.to_box(
+            self._constant_objects, self._object_centric_env.type_features,
+            dtype=self.action_space.dtype
+        )
         assert isinstance(self.observation_space, ObjectCentricBoxSpace)
         # The action space already inherits from Box, so we don't need to change it.
         assert isinstance(self.action_space, RobotActionSpace)
