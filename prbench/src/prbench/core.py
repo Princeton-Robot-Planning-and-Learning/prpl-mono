29ffa77d59db205b9dca8dfdb913352ec79ad3c8
Utkarsh Mishra <utkarsh75477@gmail.com>
2025-11-11 18:04:04 -0500

    clean up

--- prbench/src/prbench/core.py
+++ prbench/src/prbench/core.py
@@ -228,7 +228,6 @@ class ConstantObjectPRBenchEnv(gymnasium.Env[NDArray[Any], NDArray[Any]]):
                 )
                 kwargs["options"]["init_state"] = obj_centric_state
         obs, info = self._object_centric_env.reset(*args, **kwargs)
-        # check if args/kwargs have a flag to update constant objects
         assert isinstance(self.observation_space, ObjectCentricBoxSpace)
         vec_obs = self.observation_space.vectorize(obs)
         return vec_obs, info
