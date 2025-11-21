944334792456e7486b138d82b2aee4eba07d788e
Vaibhav Saxena <saxena.vaibhav96@gmail.com>
2025-11-21 00:05:15 -0500

    fix env registration

--- prbench/src/prbench/__init__.py
+++ prbench/src/prbench/__init__.py
@@ -137,9 +137,9 @@ def register_all_environments() -> None:
         config_name = task_config.stem
         robot = {"tidybot": "TidyBot3D", "rby1a": "RBY1A3D"}[config_name.split("-")[0]]
         scene_type = config_name.split("-")[1]
-        num_objects_int = int(config_name.split("-o")[-1])
+        num_objects = int(config_name.split("-")[2][1:])
         register(
-            id=f"prbench/{robot}-{scene_type}-o{num_objects_int}-v0",
+            id=f"prbench/{robot}-{scene_type}-o{num_objects}-v0",
             entry_point=f"prbench.envs.dynamic3d.tidybot3d:{robot}Env",
             kwargs={
                 "scene_type": scene_type,
