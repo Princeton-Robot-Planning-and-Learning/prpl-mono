27e56c4a6a90e40dba661902b97f71764bd1e536
Yixuan Huang <yixuanhuang98@gmail.com>
2025-11-22 12:48:08 -0500

    add get_object_names for object centric state

--- relational-structs/src/relational_structs/object_centric_state.py
+++ relational-structs/src/relational_structs/object_centric_state.py
@@ -72,6 +72,10 @@ class ObjectCentricState:
         """Return objects of the given type in the order of __iter__()."""
         return [o for o in self if o.is_instance(object_type)]
 
+    def get_object_names(self) -> List[str]:
+        """Return the names of all objects in the state."""
+        return [o.name for o in self]
+
     def get_object_from_name(self, name: str) -> Object:
         """Look up an object from its name."""
         matches = [o for o in self if o.name == name]
