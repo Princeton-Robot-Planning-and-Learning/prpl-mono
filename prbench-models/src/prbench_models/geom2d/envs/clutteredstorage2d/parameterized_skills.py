ebd2ea79003007826e9d9247f5196ee8b254251e
Bowen Li <bowenli1024@gmail.com>
2025-11-23 21:13:45 -0500

    ready for large scale exp

--- prbench-models/src/prbench_models/geom2d/envs/clutteredstorage2d/parameterized_skills.py
+++ prbench-models/src/prbench_models/geom2d/envs/clutteredstorage2d/parameterized_skills.py
@@ -20,8 +20,6 @@ from prbench.envs.geom2d.utils import (
     get_suctioned_objects,
     get_tool_tip_position,
     run_motion_planning_for_crv_robot,
-    snap_suctioned_objects,
-    state_2d_has_collision,
 )
 from relational_structs import (
     Object,
