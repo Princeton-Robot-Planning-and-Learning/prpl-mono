3528383958e7f7fcec833d31297afa0eb2dd7345
Tom Silver <tomssilver@gmail.com>
2025-12-10 12:23:05 -0500

    wip

--- prbench/tests/envs/geom3d/test_obstruction3d.py
+++ prbench/tests/envs/geom3d/test_obstruction3d.py
@@ -35,7 +35,7 @@ def test_obstruction3d_env():
     # Uncomment to debug.
     # import pybullet as p
     # while True:
-    #     p.getMouseEvents(env.physics_client_id)
+    #     p.getMouseEvents(env._object_centric_env.physics_client_id)
 
 
 def test_pick_place_no_obstructions():
@@ -68,7 +68,7 @@ def test_pick_place_no_obstructions():
     pre_grasp_pose = Pose.from_rpy((x, y, z + dz), (np.pi, 0, np.pi / 2))
     joint_plan = run_smooth_motion_planning_to_pose(
         pre_grasp_pose,
-        sim.robot,
+        sim.robot.arm,
         collision_ids=sim._get_collision_object_ids(),  # pylint: disable=protected-access
         end_effector_frame_to_plan_frame=Pose.identity(),
         seed=123,
@@ -78,7 +78,7 @@ def test_pick_place_no_obstructions():
 
     # Make sure we stay below the required max_action_mag by a fair amount.
     joint_plan = remap_joint_position_plan_to_constant_distance(
-        joint_plan, sim.robot, max_distance=config.max_action_mag / 2
+        joint_plan, sim.robot.arm, max_distance=config.max_action_mag / 2
     )
 
     for target_joints in joint_plan[1:]:
@@ -103,7 +103,7 @@ def test_pick_place_no_obstructions():
 
     # Move up slightly to break contact with the table.
     sim.set_state(obs)
-    current_end_effector_pose = sim.robot.get_end_effector_pose()
+    current_end_effector_pose = sim.robot.arm.get_end_effector_pose()
     post_grasp_pose = Pose(
         (
             current_end_effector_pose.position[0],
@@ -112,18 +112,18 @@ def test_pick_place_no_obstructions():
         ),
         current_end_effector_pose.orientation,
     )
-    joint_distance_fn = create_joint_distance_fn(sim.robot)
+    joint_distance_fn = create_joint_distance_fn(sim.robot.arm)
     joint_plan = smoothly_follow_end_effector_path(
-        sim.robot,
+        sim.robot.arm,
         [current_end_effector_pose, post_grasp_pose],
-        sim.robot.get_joint_positions(),
+        sim.robot.arm.get_joint_positions(),
         collision_ids=set(),
         joint_distance_fn=joint_distance_fn,
         max_smoothing_iters_per_step=max_candidate_plans,
     )
 
     joint_plan = remap_joint_position_plan_to_constant_distance(
-        joint_plan, sim.robot, max_distance=config.max_action_mag / 2
+        joint_plan, sim.robot.arm, max_distance=config.max_action_mag / 2
     )
 
     for target_joints in joint_plan[1:]:
@@ -166,21 +166,21 @@ def test_pick_place_no_obstructions():
     # We don't really have to motion plan here because there are no other objects, but
     # in general we would motion plan.
     sim.set_state(obs)
-    current_end_effector_pose = sim.robot.get_end_effector_pose()
+    current_end_effector_pose = sim.robot.arm.get_end_effector_pose()
     joint_plan = smoothly_follow_end_effector_path(
-        sim.robot,
+        sim.robot.arm,
         [
             current_end_effector_pose,
             end_effector_pre_placement_pose,
             end_effector_placement_pose,
         ],
-        sim.robot.get_joint_positions(),
+        sim.robot.arm.get_joint_positions(),
         collision_ids=set(),
         joint_distance_fn=joint_distance_fn,
         max_smoothing_iters_per_step=max_candidate_plans,
     )
     joint_plan = remap_joint_position_plan_to_constant_distance(
-        joint_plan, sim.robot, max_distance=config.max_action_mag / 2
+        joint_plan, sim.robot.arm, max_distance=config.max_action_mag / 2
     )
 
     for target_joints in joint_plan[1:]:
