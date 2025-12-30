9f36bcacbeaa32c1e577a8fafb5d5dedd9724740
Yixuan Huang <yixuanhuang98@gmail.com>
2025-12-30 03:35:22 -0500

    place controller

--- prbench-models/tests/geom3d/ground3d/test_ground3d_parameterized_skills.py
+++ prbench-models/tests/geom3d/ground3d/test_ground3d_parameterized_skills.py
@@ -17,9 +17,7 @@ prbench.register_all_environments()
 def test_pick_controller():
     """Test pick controller in Ground3D environment."""
 
-    env = prbench.make(
-        "prbench/Ground3D-o1-v0", render_mode="rgb_array", use_gui=False
-    )
+    env = prbench.make("prbench/Ground3D-o1-v0", render_mode="rgb_array", use_gui=False)
     if MAKE_VIDEOS:
         env = RecordVideo(env, "unit_test_videos", name_prefix="Ground3D")
 
@@ -52,5 +50,66 @@ def test_pick_controller():
             break
     else:
         assert False, "Controller did not terminate"
+
+    env.close()
+
+
+def test_pick_and_place_controller():
+    """Test pick and place controller in Ground3D environment."""
+
+    env = prbench.make("prbench/Ground3D-o1-v0", render_mode="rgb_array", use_gui=False)
+    if MAKE_VIDEOS:
+        env = RecordVideo(env, "unit_test_videos", name_prefix="Ground3D")
+
+    obs, _ = env.reset(seed=123)
+    assert isinstance(env.observation_space, ObjectCentricBoxSpace)
+    state = env.observation_space.devectorize(obs)
+
+    sim = ObjectCentricGround3DEnv(num_cubes=1)
+    controllers = create_lifted_controllers(
+        env.action_space,
+        sim,
+    )
+    lifted_controller = controllers["pick"]
+    robot = state.get_object_from_name("robot")
+    target = state.get_object_from_name("cube0")
+    object_parameters = (robot, target)
+    controller = lifted_controller.ground(object_parameters)
+
+    rng = np.random.default_rng(123)
+    params = controller.sample_parameters(state, rng)
+
+    controller.reset(state, params)
+    for _ in range(500):
+        action = controller.step()
+        obs, _, _, _, _ = env.step(action)
+        next_state = env.observation_space.devectorize(obs)
+        controller.observe(next_state)
+        state = next_state
+        if controller.terminated():
+            break
+    else:
+        assert False, "Controller did not terminate"
+
+    lifted_controller = controllers["place"]
+    robot = state.get_object_from_name("robot")
+    target = state.get_object_from_name("cube0")
+    object_parameters = (robot, target)
+    controller = lifted_controller.ground(object_parameters)
+
+    rng = np.random.default_rng(123)
+    params = controller.sample_parameters(state, rng)
+
+    controller.reset(state, params)
+    for _ in range(500):
+        action = controller.step()
+        obs, _, _, _, _ = env.step(action)
+        next_state = env.observation_space.devectorize(obs)
+        controller.observe(next_state)
+        state = next_state
+        if controller.terminated():
+            break
+    # else:
+    #     assert False, "Controller did not terminate"
 
     env.close()
