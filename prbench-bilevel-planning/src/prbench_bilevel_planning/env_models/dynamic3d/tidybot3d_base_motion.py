799af318923bec175682cced74358a7de894e4ed
Vaibhav Saxena <saxena.vaibhav96@gmail.com>
2026-01-06 08:09:40 -0500

    Removed `render_images` kwarg, fixed and added tests (#270)
    * removed render_images kwarg, fixed and added tests
    
    * fix tests
    
    * fix lint
    
    * fix lint
    
    * fix lint

--- prbench-bilevel-planning/src/prbench_bilevel_planning/env_models/dynamic3d/tidybot3d_base_motion.py
+++ prbench-bilevel-planning/src/prbench_bilevel_planning/env_models/dynamic3d/tidybot3d_base_motion.py
@@ -41,7 +41,6 @@ def create_bilevel_planning_models(
     sim = ObjectCentricTidyBot3DEnv(
         scene_type="base_motion",
         num_objects=1,
-        render_images=False,
     )
 
     # Need to call reset to initialize the qpos, qvel.
