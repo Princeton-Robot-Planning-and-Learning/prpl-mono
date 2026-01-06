722142306d201f249d816dccafb2dc3942fa25b7
Yixuan Huang <yixuanhuang98@gmail.com>
2026-01-05 22:47:54 -0500

    add parameterized skills for table box environment

--- prbench/src/prbench/__init__.py
+++ prbench/src/prbench/__init__.py
@@ -142,6 +142,16 @@ def register_all_environments() -> None:
             entry_point="prbench.envs.geom3d.table3d:Table3DEnv",
             kwargs={"num_cubes": num_cube},
         )
+    
+    # TableBox3D environment.
+    num_cubes = [1, 2]
+    num_boxes = 1
+    for num_cube in num_cubes:
+        _register(
+            id=f"prbench/TableBox3D-o{num_cube}-v0",
+            entry_point="prbench.envs.geom3d.tablebox3d:TableBox3DEnv",
+            kwargs={"num_cubes": num_cube, "num_boxes": num_boxes},
+        )
 
     # Shelf3D environment.
     num_cubes = [1, 2, 3, 5, 10]
