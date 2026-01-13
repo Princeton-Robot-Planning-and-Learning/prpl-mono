8f0daaa91c6ee3db2c99e65c916a5d807c4d16fc
Yixuan Huang <yixuanhuang98@gmail.com>
2026-01-13 15:21:34 -0500

    finish collision checkings audit for obstruction3d and ground3d

--- prbench/src/prbench/envs/geom3d/obstruction3d.py
+++ prbench/src/prbench/envs/geom3d/obstruction3d.py
@@ -361,9 +361,12 @@ class ObjectCentricObstruction3DEnv(
     def _get_collision_object_ids(self) -> set[int]:
         assert self._target_block_id is not None
         assert self._target_region_id is not None
-        return {self._target_block_id, self._target_region_id, self.table_id} | set(
+        collision_ids = {self._target_block_id, self._target_region_id, self.table_id} | set(
             self._obstruction_ids.values()
         )
+        if self._grasped_object_id is not None:
+            collision_ids.discard(self._grasped_object_id)
+        return collision_ids
 
     def _get_movable_object_names(self) -> set[str]:
         return {"target_block"} | set(self._obstruction_ids)
