ff98b482edc85ace717d5c0dfbefd1fb13a92a89
Yixuan Huang <yixuanhuang98@gmail.com>
2026-01-15 11:48:16 -0500

    wip

--- prbench/src/prbench/envs/geom3d/tablebox3d.py
+++ prbench/src/prbench/envs/geom3d/tablebox3d.py
@@ -57,7 +57,7 @@ class TableBox3DEnvConfig(Geom3DEnvConfig, metaclass=FinalConfigMeta):
     block_rgba: tuple[float, float, float, float] = PURPLE + (1.0,)
 
     # Box.
-    box_half_extents: tuple[float, float, float] = (0.15, 0.15, 0.1)
+    box_half_extents: tuple[float, float, float] = (0.1, 0.1, 0.1)
     box_rgba: tuple[float, float, float, float] = PURPLE + (1.0,)
     box_wall_thickness: float = 0.01
 
@@ -295,10 +295,8 @@ class ObjectCentricTableBox3DEnv(
 
     def _get_collision_object_ids(self) -> set[int]:
         collision_ids = (
-            {self.table_id} | set(self._cubes.values()) | set(self._boxes.values())
+            {self.table_id} # | set(self._cubes.values()) | set(self._boxes.values())
         )
-        if self._grasped_object_id is not None:
-            collision_ids.discard(self._grasped_object_id)
         return collision_ids
 
     def _get_movable_object_names(self) -> set[str]:
