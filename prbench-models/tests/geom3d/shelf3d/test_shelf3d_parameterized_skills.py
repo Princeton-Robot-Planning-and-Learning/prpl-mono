e6a482b2e80cfdd5538ee97a82d4bb294e4e4f06
Tom Silver <tomssilver@gmail.com>
2026-01-18 16:56:21 -0500

    wip

--- prbench-models/tests/geom3d/shelf3d/test_shelf3d_parameterized_skills.py
+++ prbench-models/tests/geom3d/shelf3d/test_shelf3d_parameterized_skills.py
@@ -75,7 +75,7 @@ def test_pick_and_place_controller():
     assert isinstance(env.observation_space, ObjectCentricBoxSpace)
     state = env.observation_space.devectorize(obs)
 
-    sim = ObjectCentricShelf3DEnv(num_cubes=1, use_gui=True)
+    sim = ObjectCentricShelf3DEnv(num_cubes=1)
     controllers = create_lifted_controllers(
         env.action_space,
         sim,
