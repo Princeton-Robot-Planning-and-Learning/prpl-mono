48f642f723c713151a7401cc2038f2100e38a5c2
Yixuan Huang <yixuanhuang98@gmail.com>
2026-01-19 00:55:03 -0500

    update a bit about toss

--- prbench-models/tests/dynamic3d/ground/test_parameterized_skills.py
+++ prbench-models/tests/dynamic3d/ground/test_parameterized_skills.py
@@ -1208,7 +1208,15 @@ def test_pick_toss():
         state = next_state
         if controller.terminated():
             break
-    else:
-        assert False, "Controller did not terminate"
+    # else:
+    #     assert False, "Controller did not terminate"
+    cube_position = [state.get(cube, "x"), state.get(cube, "y"), state.get(cube, "z")]
+    cube_orientation = [state.get(cube, "qx"), state.get(cube, "qy"), state.get(cube, "qz"), state.get(cube, "qw")]
+    robot_base_position = [state.get(robot, "pos_base_x"), state.get(robot, "pos_base_y")]
+    distance = np.linalg.norm(np.array(cube_position[:2]) - np.array(robot_base_position[:2]))
+    print("cube_position", cube_position)
+    print("cube_orientation", cube_orientation)
+    print("robot base position", robot_base_position)
+    print("distance", distance)
 
     env.close()
