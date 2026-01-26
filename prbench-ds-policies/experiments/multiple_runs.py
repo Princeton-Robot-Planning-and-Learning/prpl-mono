e7d33a7150de947d1225b4b34177eaef971f5a77
Yixuan Huang <yixuanhuang98@gmail.com>
2026-01-25 21:53:54 -0500

    wip

--- prbench-ds-policies/experiments/multiple_runs.py
+++ prbench-ds-policies/experiments/multiple_runs.py
@@ -9,7 +9,7 @@ def run_experiment(env_name, seed):
     ])
 
 if __name__ == "__main__":
-    num_experiments = 110
+    num_experiments = 40
     env_name = "transport3d-o2"
-    for seed in range(num_experiments):
+    for seed in range(220, 220 + num_experiments):
         run_experiment(env_name, seed)
