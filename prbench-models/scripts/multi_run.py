d508cb65194e69a2b05535f59538930a41a69803
Yixuan Huang <yixuanhuang98@gmail.com>
2026-01-26 22:20:21 -0500

    upload 104 demonstrations for shelf3d task

--- prbench-models/scripts/multi_run.py
+++ prbench-models/scripts/multi_run.py
@@ -1,13 +1,14 @@
 import subprocess
 
-def run_experiment(env_name, seed):
+def run_experiment(seed):
     subprocess.run([
         "python",
         "scripts/planning_data_dynamics3d_prbench.py",
-        f"seed={seed}",
+        "--seed",
+        str(seed),
     ])
 
 if __name__ == "__main__":
-    num_experiments = 100
-    for seed in range(num_experiments):
+    num_experiments = 50
+    for seed in range(220, 220 + num_experiments):
         run_experiment(seed)
