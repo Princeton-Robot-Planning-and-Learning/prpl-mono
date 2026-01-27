84cec3d76f9c6cb05bd9f6caadb409068749f3ec
Yixuan Huang <yixuanhuang98@gmail.com>
2026-01-27 18:31:58 -0500

    add geom3d perceiver

---
+++ prpl-tidybot/scripts/test_geom3d_pick.py
@@ -0,0 +1,118 @@
+"""Tests for real world base-arm motion planning."""
+
+import math
+import time
+
+import numpy as np
+import prbench
+from gymnasium.wrappers import RecordVideo
+from prbench_models.geom3d.ground3d.parameterized_skills import (
+    create_lifted_controllers,
+)
+from relational_structs.spaces import ObjectCentricBoxSpace
+from spatialmath import SE2
+
+from prbench.envs.geom3d.ground3d import ObjectCentricGround3DEnv
+
+from prpl_tidybot.base_movement import reach_target_pose
+from prpl_tidybot.constants import POLICY_CONTROL_PERIOD
+from prpl_tidybot.coord_converter import CoordFrameConverter
+from prpl_tidybot.interfaces.interface import RealInterface
+from prpl_tidybot.perceivers.prbench_ground_perceiver import PRBenchGeom3DPerceiver
+from prpl_tidybot.structs import TidyBotAction
+
+prbench.register_all_environments()
+
+
+def real2sim() -> None:
+    """Test move-base-arm to the target object in ground environment with 1 cube."""
+
+    try:
+        # Create the environment.
+        num_cubes = 1
+        env = prbench.make(
+            f"prbench/Ground3D-o{num_cubes}-v0", render_mode="rgb_array"
+        )
+
+        env = RecordVideo(
+            env, "unit_test_videos", name_prefix=f"Ground3D-o{num_cubes}"
+        )
+
+        # Reset the environment and get the initial state.
+        obs, _ = env.reset(seed=125)  # type: ignore
+        assert isinstance(env.observation_space, ObjectCentricBoxSpace)
+
+        ### real interface
+        interface = RealInterface()
+
+        # initialization
+        pose_map = SE2(0, 0, 0)
+        pose_odom = SE2(0, 0, 0)
+        map_to_odom_converter = CoordFrameConverter(pose_map, pose_odom)
+        odom_to_map_converter = CoordFrameConverter(pose_odom, pose_map)
+
+        # get initial pose
+        observation = interface.get_observation()
+        map_to_odom_converter.update(observation.map_base_pose, observation.base_pose)
+        odom_to_map_converter.update(observation.base_pose, observation.map_base_pose)
+
+        perceiver = PRBenchGeom3DPerceiver(interface)
+        state = perceiver.get_state()
+        print('state', state)
+        import ipdb; ipdb.set_trace()
+        env.unwrapped._object_centric_env.set_state(state)  # type: ignore # pylint: disable=protected-access
+
+        sim = ObjectCentricGround3DEnv(num_cubes=num_cubes)
+        controllers = create_lifted_controllers(
+            env.action_space,
+            sim,
+        )
+        lifted_controller = controllers["pick"]
+        robot = state.get_object_from_name("robot")
+        target = state.get_object_from_name("cube0")
+        object_parameters = (robot, target)
+        controller = lifted_controller.ground(object_parameters)
+
+        rng = np.random.default_rng(123)
+        params = controller.sample_parameters(state, rng)
+
+        controller.reset(state, params)
+        for _ in range(500):
+            action = controller.step()
+            obs, _, _, _, _ = env.step(action)
+            next_state = env.observation_space.devectorize(obs)
+            controller.observe(next_state)
+            state = next_state
+            if controller.terminated():
+                break
+        else:
+            assert False, "Controller did not terminate"
+
+        lifted_controller = controllers["place"]
+        robot = state.get_object_from_name("robot")
+        target = state.get_object_from_name("cube0")
+        object_parameters = (robot, target)
+        controller = lifted_controller.ground(object_parameters)
+
+        rng = np.random.default_rng(123)
+        params = controller.sample_parameters(state, rng)
+
+        controller.reset(state, params)
+        for _ in range(500):
+            action = controller.step()
+            obs, _, _, _, _ = env.step(action)
+            next_state = env.observation_space.devectorize(obs)
+            controller.observe(next_state)
+            state = next_state
+            if controller.terminated():
+                break
+        else:
+            assert False, "Controller did not terminate"
+
+    finally:
+        env.close()  # type: ignore
+        interface.close()
+
+
+if __name__ == "__main__":
+    real2sim()
--- prpl-tidybot/src/prpl_tidybot/perceivers/prbench_ground_perceiver.py
+++ prpl-tidybot/src/prpl_tidybot/perceivers/prbench_ground_perceiver.py
@@ -5,6 +5,12 @@ from prbench.envs.dynamic3d.object_types import (
     MujocoObjectTypeFeatures,
     MujocoTidyBotRobotObjectType,
 )
+from prbench.envs.geom3d.object_types import (
+    Geom3DCuboidType,
+    Geom3DEnvTypeFeatures,
+    Geom3DRobotType,
+)
+from prbench.envs.geom3d.ground3d import Ground3DObjectCentricState
 from relational_structs import Object, ObjectCentricState
 from relational_structs.utils import create_state_from_dict
 
@@ -78,3 +84,65 @@ class PRBenchGroundPerceiver(Perceiver[ObjectCentricState]):
         }
 
         return create_state_from_dict(state_dict, MujocoObjectTypeFeatures)
+
+class PRBenchGeom3DPerceiver(Perceiver[ObjectCentricState]):
+    """A perceiver for the PRBench Geom3D environment."""
+
+    def __init__(self, interface: Interface) -> None:
+        self._interface = interface
+
+    def get_state(self) -> ObjectCentricState:
+        state_dict: dict[Object, dict[str, float]] = {}
+
+        # Extract the robot state.
+        qpos_base = self._interface.get_map_base_state()
+        qpos_arm = self._interface.get_arm_state()
+        gripper_state = self._interface.get_gripper_state()
+
+        # Add robot into object-centric state.
+        robot = Object("robot", Geom3DRobotType)
+
+        # Build this super explicitly, even though verbose, to be careful.
+        state_dict[robot] = {
+            "pos_base_x": qpos_base.x,
+            "pos_base_y": qpos_base.y,
+            "pos_base_rot": qpos_base.theta(),
+            "joint_1": qpos_arm[0],
+            "joint_2": qpos_arm[1],
+            "joint_3": qpos_arm[2],
+            "joint_4": qpos_arm[3],
+            "joint_5": qpos_arm[4],
+            "joint_6": qpos_arm[5],
+            "joint_7": qpos_arm[6],
+            "finger_state": gripper_state,
+            # NOTE: velocity not actually used or measured in real.
+            "grasp_active": 0.0,
+            "grasp_tf_x": 0.0,
+            "grasp_tf_y": 0.0,
+            "grasp_tf_z": 0.0,
+            "grasp_tf_qx": 0.0,
+            "grasp_tf_qy": 0.0,
+            "grasp_tf_qz": 0.0,
+            "grasp_tf_qw": 1.0,
+        }
+
+        # Placeholder for actual object detection! Coming soon!!!
+        cube = Object("cube0", Geom3DCuboidType)
+        state_dict[cube] = {
+            "pose_x": 0.0,
+            "pose_y": 0.0,
+            "pose_z": 0.0,
+            "pose_qx": 0.0,
+            "pose_qy": 0.0,
+            "pose_qz": 0.0,
+            "pose_qw": 1.0,
+            "grasp_active": 0.0,
+            "object_type": 0,
+            "half_extent_x": 0.03,
+            "half_extent_y": 0.03,
+            "half_extent_z": 0.03,
+        }
+
+        return create_state_from_dict(
+            state_dict, Geom3DEnvTypeFeatures, state_cls=Ground3DObjectCentricState
+        )
