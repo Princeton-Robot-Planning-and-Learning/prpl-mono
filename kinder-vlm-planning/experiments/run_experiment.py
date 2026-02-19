9cfde9f24a16f5a348f0d2b68074654e9291a2d7
Yixun-Hu <yixunhu21@gmail.com>
2026-02-19 15:29:09 -0500

    add motion2d config and update run_experiment scripts

--- kinder-vlm-planning/experiments/run_experiment.py
+++ kinder-vlm-planning/experiments/run_experiment.py
@@ -2,8 +2,8 @@
 
 Examples:
 - Running on a single environment with a single seed:
-    python experiments/run_experiment.py env=Motion2D-p0-v0 seed=0 vlm_model=gpt-5 \
-        temperature=1
+    python experiments/run_experiment.py -m env=Motion2D-p1-v0 seed=0 vlm_model=gpt-5 \
+        rgb_observation=true,false temperature=1
     python experiments/run_experiment.py -m env=StickButton2D-b1-v0 seed=0 \
         vlm_model=gpt-5 temperature=1
 
@@ -11,7 +11,7 @@ Examples:
     python experiments/run_experiment.py -m seed='range(0,3)' \
         env=Motion2D-p0-v0,StickButton2D-b1-v0 \
         vlm_model=gpt-5 rgb_observation=true,false temperature=1
-    python exeriments/run_experiment.py -m seed='range(0,3)' \
+    python experiments/run_experiment.py -m seed='range(0,3)' \
         env=BaseMotion3D-v0,Transport3D-o2-v0,Shelf3D-o1-v0 \
         vlm_model=gpt-5 rgb_observation=true,false temperature=1 hydra/launcher=joblib
 """
