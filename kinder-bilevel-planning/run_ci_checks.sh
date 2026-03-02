70fef43fd2c396e6ca8d222191ef6b80f1f42950
Tom Silver <tomssilver@gmail.com>
2026-03-02 10:44:42 -0500

    wip

--- kinder-bilevel-planning/run_ci_checks.sh
+++ kinder-bilevel-planning/run_ci_checks.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ./run_autoformat.sh
 mypy .
-pytest . --pylint -m pylint --pylint-rcfile=.pylintrc --ignore=notebooks
+pytest . --pylint -m pylint --pylint-rcfile=.pylintrc
 pytest tests/
-pytest notebooks/ --nbmake --nbmake-timeout=120
