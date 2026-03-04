ae4f987f3f3c1f7157ee8780ec739caa909a3c2a
Tom Silver <tomssilver@gmail.com>
2026-03-04 15:40:04 -0500

    wip

--- bilevel-planning/tests/test_bilevel_planning_graph.py
+++ bilevel-planning/tests/test_bilevel_planning_graph.py
@@ -1,8 +1,5 @@
 """Tests for bilevel_planning_graph.py."""
 
-import tempfile
-from pathlib import Path
-
 import numpy as np
 
 from bilevel_planning.bilevel_planning_graph import BilevelPlanningGraph
@@ -63,35 +60,3 @@ def test_bilevel_planning_graph():
 
     # Uncomment to make GIF.
     # bpg.render_gif(save_path="debug.gif")
-
-
-def test_render_image():
-    """Test that render_image() produces a PNG file."""
-    bpg = BilevelPlanningGraph()
-    states = [["s0"], ["s1"], ["s2"], ["s3"]]
-    for s in states:
-        bpg.add_state_node(s)
-    bpg.add_action_edge(states[0], [0], states[1])
-    bpg.add_action_edge(states[1], [1], states[2])
-    bpg.add_action_edge(states[2], [2], states[3])
-
-    abs1, abs2 = ("a",), ("b",)
-    bpg.add_abstract_state_node(abs1)
-    bpg.add_abstract_state_node(abs2)
-    bpg.add_abstract_action_edge(abs1, "go", abs2)
-    bpg.add_state_abstractor_edge(states[0], abs1)
-    bpg.add_state_abstractor_edge(states[1], abs1)
-    bpg.add_state_abstractor_edge(states[2], abs2)
-    bpg.add_state_abstractor_edge(states[3], abs2)
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-        path = Path(tmpdir) / "test.png"
-        bpg.render_image(save_path=path, final_state=states[3])
-        assert path.exists()
-        assert path.stat().st_size > 0
-
-        # Test with max_concrete_states subsampling.
-        path2 = Path(tmpdir) / "test_subsampled.png"
-        bpg.render_image(save_path=path2, final_state=states[3], max_concrete_states=2)
-        assert path2.exists()
-        assert path2.stat().st_size > 0
